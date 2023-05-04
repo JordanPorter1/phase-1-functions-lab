02275ac168dcadccc5dbd749b147055e4d369902
Jordan Porter <s.jordanporter@hotmail.com>
2023-05-04 08:33:27 -0600

    final

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    let hqBlock = 42;
+    return Math.abs(blockNumber - hqBlock);
+  }
+
+function distanceFromHqInFeet(blockNumber) {
+    let hqBlock = 42;
+    return Math.abs((blockNumber - hqBlock) * 264);
+}
+
+function distanceTravelledInFeet(startPoint, endPoint) {
+    return Math.abs((startPoint - endPoint) * 264);
+}
+
+function calculatesFarePrice(startPoint, endPoint) {
+    if (distanceTravelledInFeet(startPoint, endPoint) < 400) {
+    return 0;}
+    else if (distanceTravelledInFeet(startPoint, endPoint) <= 2000) {
+    return (distanceTravelledInFeet(startPoint, endPoint) - 400) * 0.02;}
+    else if (distanceTravelledInFeet(startPoint, endPoint) >= 2000 && distanceTravelledInFeet(startPoint, endPoint) <= 2500) {
+    return 25;}
+    else if (distanceTravelledInFeet(startPoint, endPoint) > 2500) {
+    return 'cannot travel that far';}
+}
+
+
+//(age >= 18 && age <= 21)
